6be48b704b87435dad8a47a57b0811f75dbba727
WoldemarK <kovtynov.vladimir@gmail.com>
2022-11-27 20:03:12 +0300

    new adding

---
+++ src/main/java/org/example/model/Director.java
@@ -0,0 +1,69 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+
+    @OneToMany(mappedBy = "director")
+    private List<Movie> moves;
+
+    public Director() {
+    }
+
+    public Director(String name, int age, List<Movie> moves) {
+        this.name = name;
+        this.age = age;
+        this.moves = moves;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Movie> getMoves() {
+        return moves;
+    }
+
+    public void setMoves(List<Movie> moves) {
+        this.moves = moves;
+    }
+
+    @Override
+    public String toString() {
+        return "Director{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+       ", moves=" + moves +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/model/Movie.java
@@ -0,0 +1,70 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @Column(name = "year_of_production")
+    private int yearOfProduction;
+
+    @ManyToOne
+    @JoinColumn(name = "director_id", referencedColumnName = "id")
+    private Director director;
+
+    public Movie() {
+    }
+
+    public Movie(String name, int yearOfProduction, Director director) {
+        this.name = name;
+        this.yearOfProduction = yearOfProduction;
+        this.director = director;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", year_of_production=" + yearOfProduction +
+          //  ", director=" + director +
+                '}';
+    }
+}
